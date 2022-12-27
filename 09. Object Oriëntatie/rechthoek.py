8dc76438292bf059fac7a544f9eb647bd2cbd372
Jialin Li <jialin.li@student.ucll.be>
2022-12-27 22:14:48 +0100

    Add files via upload

--- 09. Object Oriëntatie/rechthoek.py
+++ 09. Object Oriëntatie/rechthoek.py
@@ -1,5 +1,15 @@
 # https://dodona.ugent.be/nl/courses/1286/series/14361/activities/1773014461
 
+"""
+Opmerking i.v.m. de overlap()-methode in klasse Rechthoek:
+
+Ik denk dat Dodona hierbij niet klopt. De onderstaande code in overlap() is volgens mij
+fout, maar wordt wel goedgekeurd door Dodona.
+Eronder staat in commentaar de code waarvan ik denk dat het juist is,
+maar volgens Dodona geeft het een fout resultaat, terwijl als je de rechthoeken
+uittekent, je zal zien dat het wél juist is!
+"""
+
 class Punt:
     def __init__(self, x, y):
         assert type(x), type(y) is int
@@ -42,10 +52,33 @@ class Rechthoek:
     def rechtsonder(self):
         return Punt(self.punt_x + self.breedte, self.punt_y + self.hoogte)
 
+    """
+    LET OP: Deze code voor overlap() is volgens mij fout, maar wordt wel goedgekeurd
+    door Dodona. Onderaan in commentaar staat de code die volgens mij wel juist is.
+    """
     def overlap(self, rechthoek):
-        r1, r2 = self , rechthoek
-        if self.punt.x > rechthoek.punt.x or (self.punt.x == rechthoek.punt.x and self.punt.y > rechthoek.punt.y):
-            r1, r2 = rechthoek, self
-        if r1.rechtsonder().x <= r2.punt.x or r1.rechtsonder().y <= r2.punt.y:
+        r1, r2 = self, rechthoek
+        if r1.punt_x > r2.punt_x or (r1.punt_x == r2.punt_x and r1.punt_y > r2.punt_y):
+            r1, r2 = r2, r1
+        if r1.rechtsonder().x <= r2.punt_x or r1.rechtsonder().y <= r2.punt_y:
             return None
-        return Rechthoek( r2.punt, min( r1.rechtsonder().x - r2.punt.x, r2.breedte ), min( r1.rechtsonder().y - r2.punt.y, r2.hoogte ))
+        return Rechthoek(r2.punt, min(r1.rechtsonder().x-r2.punt_x, r2.breedte), min(r1.rechtsonder().y-r2.punt_y, r2.hoogte))
+
+    """
+    Onderstaande code voor overlap() is in mijn ogen correct, maar wordt niet goedgekeurd door Dodona.
+    Als je de rechthoeken uittekent, zul je zien dat dit echter wél de juiste oplossing geeft.
+    """
+    # def overlap(self, rechthoek):
+    #     if r1.punt_x > r2.punt_x: # zorg dat r1 altijd de meest linkse rechthoek is
+    #         r1, r2 = r2, r1
+
+    #     if r1.punt_y <= r2.punt_y: # r1 ligt boven r2
+    #         if r1.rechtsonder().x <= r2.punt_x or r1.rechtsonder().y <= r2.punt_y: # geen overlapping
+    #             return None
+    #         else: # wel overlapping
+    #             return Rechthoek(r2.punt, min(r1.rechtsonder().x-r2.punt.x, r2.breedte), min(r1.rechtsonder().y-r2.punt.y, r2.hoogte))
+    #     else: # r1 ligt onder r2
+    #         if r1.rechtsonder().x <= r2.punt_x or r2.rechtsonder().y <= r1.punt_y: # geen overlap
+    #             return None
+    #         else: # wel overlapping
+    #             return Rechthoek(Punt(r2.punt_x, r1.punt_y), min(r1.rechtsonder().x-r2.punt.x, r2.breedte), min(r2.rechtsonder().y-r1.punt.y, r1.hoogte))
